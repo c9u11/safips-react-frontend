37734b1e68d799273061ae7c0d1153f0c58a5544
c9u11 <tjrdud6412@naver.com>
2025-10-18 21:59:54 +0900

    Demo Measurement

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,18 +4,10 @@ import {
   addSensorListeners,
   removeSensorListeners,
 } from '@/utils/sensor';
+import { useDriveStore } from '@/stores/useDriveStore';
 
 export default function Home() {
-  const [sensorData, setSensorData] = useState({
-    latitude: 0.1,
-    longitude: 0.1,
-    accelerationX: 0.1,
-    accelerationY: 0.1,
-    accelerationZ: 0.1,
-    gyroscopeX: 0.1,
-    gyroscopeY: 0.1,
-    gyroscopeZ: 0.1,
-  });
+  const { driveHistory, accelerationX, accelerationY, accelerationZ, gyroscopeX, gyroscopeY, gyroscopeZ, setAccelerationData, setGyroscopeData, startDrive, stopDrive } = useDriveStore();
   const [permissionStatus, setPermissionStatus] = useState('pending');
 
   const requestPermissions = async () => {
@@ -35,22 +27,25 @@ export default function Home() {
   };
 
   const motionHandler = (event) => {
-    setSensorData(prev => (
-      {
-        ...prev,
-        accelerationX: event.acceleration.x,
-        accelerationY: event.acceleration.y,
-        accelerationZ: event.acceleration.z,
-      }))
+    setAccelerationData({
+      accelerationX: event.acceleration.x,
+      accelerationY: event.acceleration.y,
+      accelerationZ: event.acceleration.z,
+    });
   };
   const orientationHandler = (event) => {
-    setSensorData(prev => (
-      {
-        ...prev,
-        gyroscopeX: event.alpha,
-        gyroscopeY: event.beta,
-        gyroscopeZ: event.gamma,
-      }))
+    setGyroscopeData({
+      gyroscopeX: event.alpha,
+      gyroscopeY: event.beta,
+      gyroscopeZ: event.gamma,
+    });
+  };
+
+  const startHandler = async () => {
+    await startDrive();
+  };
+  const endHandler = async () => {
+    await stopDrive();
   };
 
   useEffect(() => {
@@ -62,29 +57,95 @@ export default function Home() {
   }, [permissionStatus]);
 
   return (
-    <div>
-      <button className='bg-blue-500 text-white p-2 rounded-md' onClick={requestPermissions}>Request Permissions</button>
-      <p>
-        {permissionStatus}
-      </p>
-      <p>
-        {sensorData?.accelerationX || 'N/A'}
-      </p>
-      <p>
-        {sensorData?.accelerationY || 'N/A'}
-      </p>
-      <p>
-        {sensorData?.accelerationZ || 'N/A'}
-      </p>
-      <p>
-        {sensorData?.gyroscopeX || 'N/A'}
-      </p>
-      <p>
-        {sensorData?.gyroscopeY || 'N/A'}
-      </p>
-      <p>
-        {sensorData?.gyroscopeZ || 'N/A'}
-      </p>
+    <div className='py-3 px-10 gap-2 flex flex-col'>
+      <div className='flex gap-2'>
+        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={requestPermissions}>Request Permissions</button>
+        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={startHandler}>Start</button>
+        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={endHandler}>Stop</button>
+      </div>
+      <table className='w-full border-collapse border'>
+        <tbody>
+          <tr>
+            <th>권한 상태</th>
+            <td>{permissionStatus}</td>
+          </tr>
+          <tr>
+            <th>가속도 X</th>
+            <td>{accelerationX || 'N/A'}</td>
+          </tr>
+          <tr>
+            <th>가속도 Y</th>
+            <td>{accelerationY || 'N/A'}</td>
+          </tr>
+          <tr>
+            <th>가속도 Z</th>
+            <td>{accelerationZ || 'N/A'}</td>
+          </tr>
+          <tr>
+            <th>자이로스코프 X</th>
+            <td>{gyroscopeX || 'N/A'}</td>
+          </tr>
+          <tr>
+            <th>자이로스코프 Y</th>
+            <td>{gyroscopeY || 'N/A'}</td>
+          </tr>
+          <tr>
+            <th>자이로스코프 Z</th>
+            <td>{gyroscopeZ || 'N/A'}</td>
+          </tr>
+        </tbody>
+      </table>
+      {/* driveHistory */}
+      <div className='w-full overflow-x-auto overflow-y-auto h-130 bg-gray-100 rounded-md'>
+        <table className='w-full whitespace-nowrap text-center border-separate border-spacing-3'>
+          <thead className='sticky top-0 bg-gray-100 h-10'>
+            <tr>
+              <th>time</th>
+              <th>accX</th>
+              <th>accY</th>
+              <th>accZ</th>
+              <th>gyroX</th>
+              <th>gyroY</th>
+              <th>gyroZ</th>
+              <th>lat</th>
+              <th>lng</th>
+            </tr>
+          </thead>
+          <tbody>
+            {driveHistory.reverse().map((item) => (
+              <tr key={item.timestamp}>
+                <td>
+                  {new Date(item.timestamp).toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3 })}
+                </td>
+                <td>
+                  {item.accelerationX}
+                </td>
+                <td>
+                  {item.accelerationY}
+                </td>
+                <td>
+                  {item.accelerationZ}
+                </td>
+                <td>
+                  {item.gyroscopeX}
+                </td>
+                <td>
+                  {item.gyroscopeY}
+                </td>
+                <td>
+                  {item.gyroscopeZ}
+                </td>
+                <td>
+                  {item.latitude}
+                </td>
+                <td>
+                  {item.longitude}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
