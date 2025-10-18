5fd48d5a62b596ef11cff239aa8d80dfc5219270
c9u11 <tjrdud6412@naver.com>
2025-10-18 14:45:02 +0900

    Set API Client

---
+++ src/apis/apiClient.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export const apiClient = axios.create();
+
+export function ApiClientSetting() {
+  apiClient.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+  apiClient.defaults.headers.common["Content-Type"] = "application/json";
+
+  apiClient.interceptors.request.use((config) => {
+    return config;
+  });
+
+  apiClient.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.message) alert(error.message);
+      return Promise.reject(error);
+    }
+  );
+
+  return <></>;
+}
+
+export default apiClient;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { ApiClientSetting } from '@/apis/apiClient';
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <ApiClientSetting />
+    <App />
+  </>
 );
