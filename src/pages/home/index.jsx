f96d1005d0c3e2b1a3ab6545fa5dd0924e668cce
c9u11 <tjrdud6412@naver.com>
2025-10-18 22:47:15 +0900

    Removed unused sensor listener functions and related useEffect

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,44 +1,36 @@
-import { useEffect, useState } from 'react';
-import {
-  requestSensorPermissions,
-  addSensorListeners,
-  removeSensorListeners,
-} from '@/utils/sensor';
-import { useDriveStore } from '@/stores/useDriveStore';
+import { useState } from "react";
+import { requestSensorPermissions } from "@/utils/sensor";
+import { useDriveStore } from "@/stores/useDriveStore";
 
 export default function Home() {
-  const { driveHistory, accelerationX, accelerationY, accelerationZ, gyroscopeX, gyroscopeY, gyroscopeZ, setAccelerationData, setGyroscopeData, startDrive, stopDrive } = useDriveStore();
-  const [permissionStatus, setPermissionStatus] = useState('pending');
+  const {
+    driveHistory,
+    accelerationX,
+    accelerationY,
+    accelerationZ,
+    gyroscopeX,
+    gyroscopeY,
+    gyroscopeZ,
+    startDrive,
+    stopDrive
+  } = useDriveStore();
+  const [permissionStatus, setPermissionStatus] = useState("pending");
 
   const requestPermissions = async () => {
     let allGranted = false;
     try {
-      setPermissionStatus('requesting');
+      setPermissionStatus("requesting");
 
       const permissions = await requestSensorPermissions();
 
-      allGranted = Object.values(permissions).every(permission =>
-        permission === 'granted' || permission === 'not-supported'
+      allGranted = Object.values(permissions).every(
+        (permission) =>
+          permission === "granted" || permission === "not-supported"
       );
     } finally {
-      if (allGranted) setPermissionStatus('granted');
-      else setPermissionStatus('denied');
+      if (allGranted) setPermissionStatus("granted");
+      else setPermissionStatus("denied");
     }
-  };
-
-  const motionHandler = (event) => {
-    setAccelerationData({
-      accelerationX: event.acceleration.x,
-      accelerationY: event.acceleration.y,
-      accelerationZ: event.acceleration.z,
-    });
-  };
-  const orientationHandler = (event) => {
-    setGyroscopeData({
-      gyroscopeX: event.alpha,
-      gyroscopeY: event.beta,
-      gyroscopeZ: event.gamma,
-    });
   };
 
   const startHandler = async () => {
@@ -48,22 +40,29 @@ export default function Home() {
     await stopDrive();
   };
 
-  useEffect(() => {
-    if (permissionStatus !== 'granted') return;
-    addSensorListeners(motionHandler, orientationHandler);
-    return () => {
-      removeSensorListeners(motionHandler, orientationHandler);
-    };
-  }, [permissionStatus]);
-
   return (
-    <div className='py-3 px-10 gap-2 flex flex-col'>
-      <div className='flex gap-2'>
-        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={requestPermissions}>Request Permissions</button>
-        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={startHandler}>Start</button>
-        <button className='bg-blue-500 text-white p-2 rounded-md' onClick={endHandler}>Stop</button>
+    <div className="py-3 px-10 gap-2 flex flex-col">
+      <div className="flex gap-2">
+        <button
+          className="bg-blue-500 text-white p-2 rounded-md"
+          onClick={requestPermissions}
+        >
+          Request Permissions
+        </button>
+        <button
+          className="bg-blue-500 text-white p-2 rounded-md"
+          onClick={startHandler}
+        >
+          Start
+        </button>
+        <button
+          className="bg-blue-500 text-white p-2 rounded-md"
+          onClick={endHandler}
+        >
+          Stop
+        </button>
       </div>
-      <table className='w-full border-collapse border'>
+      <table className="w-full border-collapse border">
         <tbody>
           <tr>
             <th>권한 상태</th>
@@ -71,34 +70,34 @@ export default function Home() {
           </tr>
           <tr>
             <th>가속도 X</th>
-            <td>{accelerationX || 'N/A'}</td>
+            <td>{accelerationX || "N/A"}</td>
           </tr>
           <tr>
             <th>가속도 Y</th>
-            <td>{accelerationY || 'N/A'}</td>
+            <td>{accelerationY || "N/A"}</td>
           </tr>
           <tr>
             <th>가속도 Z</th>
-            <td>{accelerationZ || 'N/A'}</td>
+            <td>{accelerationZ || "N/A"}</td>
           </tr>
           <tr>
             <th>자이로스코프 X</th>
-            <td>{gyroscopeX || 'N/A'}</td>
+            <td>{gyroscopeX || "N/A"}</td>
           </tr>
           <tr>
             <th>자이로스코프 Y</th>
-            <td>{gyroscopeY || 'N/A'}</td>
+            <td>{gyroscopeY || "N/A"}</td>
           </tr>
           <tr>
             <th>자이로스코프 Z</th>
-            <td>{gyroscopeZ || 'N/A'}</td>
+            <td>{gyroscopeZ || "N/A"}</td>
           </tr>
         </tbody>
       </table>
       {/* driveHistory */}
-      <div className='w-full overflow-x-auto overflow-y-auto h-130 bg-gray-100 rounded-md'>
-        <table className='w-full whitespace-nowrap text-center border-separate border-spacing-3'>
-          <thead className='sticky top-0 bg-gray-100 h-10'>
+      <div className="w-full overflow-x-auto overflow-y-auto h-130 bg-gray-100 rounded-md">
+        <table className="w-full whitespace-nowrap text-center border-separate border-spacing-3">
+          <thead className="sticky top-0 bg-gray-100 h-10">
             <tr>
               <th>time</th>
               <th>accX</th>
@@ -115,32 +114,22 @@ export default function Home() {
             {driveHistory.reverse().map((item) => (
               <tr key={item.timestamp}>
                 <td>
-                  {new Date(item.timestamp).toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3 })}
+                  {new Date(item.timestamp).toLocaleTimeString([], {
+                    hour12: false,
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    fractionalSecondDigits: 3
+                  })}
                 </td>
-                <td>
-                  {item.accelerationX}
-                </td>
-                <td>
-                  {item.accelerationY}
-                </td>
-                <td>
-                  {item.accelerationZ}
-                </td>
-                <td>
-                  {item.gyroscopeX}
-                </td>
-                <td>
-                  {item.gyroscopeY}
-                </td>
-                <td>
-                  {item.gyroscopeZ}
-                </td>
-                <td>
-                  {item.latitude}
-                </td>
-                <td>
-                  {item.longitude}
-                </td>
+                <td>{item.accelerationX}</td>
+                <td>{item.accelerationY}</td>
+                <td>{item.accelerationZ}</td>
+                <td>{item.gyroscopeX}</td>
+                <td>{item.gyroscopeY}</td>
+                <td>{item.gyroscopeZ}</td>
+                <td>{item.latitude}</td>
+                <td>{item.longitude}</td>
               </tr>
             ))}
           </tbody>
