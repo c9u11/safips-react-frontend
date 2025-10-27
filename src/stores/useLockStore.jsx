33da7e5ba0c6ddef6a5396429b2f6b3985261877
c9u11 <tjrdud6412@naver.com>
2025-10-27 17:00:32 +0900

    Add wake lock handling in useLockStore

--- src/stores/useLockStore.jsx
+++ src/stores/useLockStore.jsx
@@ -1,11 +1,14 @@
 import { create } from 'zustand';
+import { wakeLockScreen } from '@/utils/screen';
 
 export const useLockStore = create((set, get) => ({
   isLocked: false,
   lockHandler: () => {
+    if (!wakeLockScreen()) alert('절전모드 방지에 실패했습니다.');
     set({ isLocked: true });
   },
   unlockHandler: () => {
+    if (!releaseWakeLockScreen()) alert('절전모드 방지 해제에 실패했습니다.');
     set({ isLocked: false });
   },
 }));
