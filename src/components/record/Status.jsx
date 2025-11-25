8707069cd373c879b391297bf1fa646b53396b2d
c9u11 <tjrdud6412@naver.com>
2025-11-25 16:50:53 +0900

    Refactor Status component to display distance, current speed, and total calories. Update useDriveStore to calculate and store these values based on sensor data.

--- src/components/record/Status.jsx
+++ src/components/record/Status.jsx
@@ -2,23 +2,23 @@ import useDriveStore from '@/stores/useDriveStore';
 import useTimer from './useTimer';
 import MainItem from './MainItem';
 import SubItem from './SubItem';
-import {LuBike} from "react-icons/lu";
+import { LuBike } from "react-icons/lu";
 import { MdOutlineSpeed } from "react-icons/md";
 import { RiTimerLine } from "react-icons/ri";
 import { AiOutlineFire } from "react-icons/ai";
 
 export default function Status() {
-  const driveStatus = useDriveStore((state) => state.driveStatus);
+  const distance = useDriveStore((state) => state.distance);
+  const currentSpeed = useDriveStore((state) => state.currentSpeed);
+  const totalCalories = useDriveStore((state) => state.totalCalories);
   const elapsed = useTimer();
   return (
     <div className='flex flex-col gap-10'>
-      {/* <p>{driveStatus === 'recording' ? '측정 중' : '측정 중지'}</p>
-      <p>시간: {elapsed}s</p> */}
-      <MainItem icon={<LuBike className='text-4xl text-[#72D9FF]'/>} title={'거리(km)'} value={'0.00'}/>
+      <MainItem icon={<LuBike className='text-4xl text-[#72D9FF]' />} title={'거리(km)'} value={distance.toFixed(2)} />
       <div className='flex w-full justify-center gap-12 px-12'>
-        <SubItem icon={<MdOutlineSpeed className='text-xl'/>} title={'평균속도'} value={`8'55"`}/>
-        <SubItem icon={<RiTimerLine className='text-xl'/>} title={'시간'} value={elapsed}/>
-        <SubItem icon={<AiOutlineFire className='text-xl'/>} title={'칼로리'} value={`170`}/>
+        <SubItem icon={<MdOutlineSpeed className='text-xl' />} title={'현재 속도'} value={currentSpeed.toFixed(2)} />
+        <SubItem icon={<RiTimerLine className='text-xl' />} title={'시간'} value={elapsed} />
+        <SubItem icon={<AiOutlineFire className='text-xl' />} title={'칼로리'} value={totalCalories.toFixed(1)} />
       </div>
     </div>
   );
